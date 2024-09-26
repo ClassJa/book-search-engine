24768505c6b94de069cc068831addab270587f2a
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-25 21:15:37 -0500

    Added the typeDef and resolver for the book engine

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [bookSchema]
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    createdAt: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    book: [Book]!
+    book(bookId: ID!): Book
+  }
+
+  type Mutation {
+    addUser(userId: ID!, username: String!, email: String!, password: String!, savedBooks: [bookSchema]): User
+    addBook(bookId: ID!, authors: String!, description: String!, createdAt: String, image: String, link: String, title: String!): Book
+    removeUser(userId: ID!): User
+    removeBook(bookId: ID!): Book
+  }
+`;
+
+module.exports = typeDefs;
